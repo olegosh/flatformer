2e737244f983fb7a0cd578a7a8192e1759c576af
Oleg Oshurkov <olegosh137@gmail.com>
2020-03-03 18:21:35 +0200

    Complete tests for sources loading

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './index.css';
 const {sources} = require('./sources');
 
-console.log(sources);
-const img = document.createElement('img');
-img.src = sources[55];
-img.addEventListener('load', () => {
-  document.body.appendChild(img);
-});
+// // console.log(sources);
+// const img = document.createElement('img');
+// img.src = sources[55];
+// img.addEventListener('load', () => {
+//   document.body.appendChild(img);
+// });
+
+for (let i = 0; i < sources.length; i += 1) {
+  let img = document.createElement('img');
+  img.src = sources[i];
+  img.addEventListener('load', () => {
+    document.body.appendChild(img);
+    img.classList.add('img');
+  });
+}
