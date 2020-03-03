25a9e68b140e5c01c01665a02683f5b9a82ab8ea
Oleg Oshurkov <olegosh137@gmail.com>
2020-03-03 18:11:29 +0200

    Added and tested sources module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './index.css';
+const {sources} = require('./sources');
 
-document.getElementById('root').textContent = 'Working...';
+console.log(sources);
+const img = document.createElement('img');
+img.src = sources[55];
+img.addEventListener('load', () => {
+  document.body.appendChild(img);
+});
