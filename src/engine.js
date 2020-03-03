cfd9b56743d01303d32474516c6ae0b8e863cca9
Oleg Oshurkov <olegosh137@gmail.com>
2020-03-03 20:15:22 +0200

    Added and tested engine module

---
+++ src/engine.js
@@ -0,0 +1,25 @@
+let game;
+
+export function startGame(callback) {
+  if (callback) {
+    game = callback;
+  }
+  loop();
+}
+function loop() {
+  try {
+    if (typeof game === 'function') {
+      game();
+    } else {
+      throw new TypeError('game variable must be a function');
+    }
+    requestAnimationFrame(loop);
+  } catch(error) {
+    throw error;
+  }
+}
+export function setStage(callback) {
+  game = callback;
+  
+  // init();
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 import './index.css';
-const {sources} = require('./sources');
-const {maps} = require('./maps');
+import sources from './sources';
+import maps from './maps';
+import { startGame, setStage } from './engine';
 
-// // console.log(sources);
-// const img = document.createElement('img');
-// img.src = sources[55];
-// img.addEventListener('load', () => {
-//   document.body.appendChild(img);
-// });
+let x = 0;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-for (let i = 0; i < sources.length; i += 1) {
-  let img = document.createElement('img');
-  img.src = sources[i];
-  img.addEventListener('load', () => {
-    document.body.appendChild(img);
-    img.classList.add('img');
-  });
-}
+const canvas = document.getElementById('canvas');
+canvas.width = width;
+canvas.height = height;
+const context = canvas.getContext('2d');
 
-console.log(maps);
+const testLevel = () => {
+  context.clearRect(0, 0, width, height);
+  context.fillStyle = '#540930';
+  context.fillRect(x++, 50, 50, 50);
+  x = x > 600 ? 0 : x;
+};
+
+// setStage(testLevel);
+startGame();
+
+// for (let i = 0; i < sources.length; i += 1) {
+//   let img = document.createElement('img');
+//   img.src = sources[i];
+//   img.addEventListener('load', () => {
+//     document.body.appendChild(img);
+//     img.classList.add('img');
+//   });
+// }
